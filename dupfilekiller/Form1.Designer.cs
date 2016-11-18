ce70ec4e62dd7a2b0d2e940cd63c1620cb3a321a
zwxscience <zwxscience@gmail.com>
2016-11-19 00:30:18 +0800

    many duplicate files in our PC ,for eg Images,muisic,etc. Sometimes we need delete them to save space. All duplicate files are move to a specific folder and you can delete them by hand.
    Signed-off-by: zwxscience <zwxscience@gmail.com>

---
+++ dupfilekiller/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace dumpfilekiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "点击选择文件夹";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 81);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(433, 178);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(227, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "选择文件夹处理重复文件";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(32, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(287, 24);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "所有的重复文件会剪切在所选择根目录文件夹的\r\ndupfileToDelete文件夹中，如有需要，请手动删除。";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 314);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "重复文件杀手";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ dupfilekiller/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+namespace dumpfilekiller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int fileIndex;
+        string dumpfilefolderName;
+        Dictionary<string, long> fileInfo;
+        long savebit;
+        int dupfiles;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //选择文件夹
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.SelectedPath = "E:\\Music";
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                //this.textbox1.Text = ;
+                DirectoryInfo theFolder = new DirectoryInfo(@"" + fbd.SelectedPath);
+                fileIndex = 0;
+                savebit = 0;
+                dupfiles = 0;
+                //dumpfilefolderName = ;
+                dumpfilefolderName = fbd.SelectedPath + "\\dupfileToDelete";
+                fileInfo = new Dictionary<string, long>();
+                dealfile(theFolder);
+                fileInfo.Clear();
+                string finalOutput = "共搜索文件 " + fileIndex + " 个,重复文件" + dupfiles + "个,节省空间共约 " + savebit / (1024 * 1024) + "M.";
+                this.textBox1.Text = finalOutput + "\r\n"+ this.textBox1.Text;
+                MessageBox.Show(finalOutput);
+            }
+            savebit = 0;
+            fileIndex = 0;
+            dupfiles = 0;
+
+        }
+        private void dealfile(DirectoryInfo theFolder)
+        {
+
+            DirectoryInfo[] dirInfo = theFolder.GetDirectories();
+            foreach (FileInfo NextFile in theFolder.GetFiles())  //遍历文件
+            {
+                fileIndex++;
+                this.label1.Text = "正在处理第 " + fileIndex + " 个文件";
+                System.Windows.Forms.Application.DoEvents();
+                if (!fileInfo.Keys.Contains(NextFile.Name))
+                {
+                    fileInfo.Add(NextFile.Name, NextFile.Length);
+                }
+                else
+                {
+                    string text = "文件" + fileIndex + ":" + NextFile.Name + ",:大小" + NextFile.Length + "字节";
+                    this.textBox1.Text += text+"重复文件\r\n";
+                    if (Directory.Exists(dumpfilefolderName) == false)//如果不存在就创建file文件夹
+                    {
+                        Directory.CreateDirectory(dumpfilefolderName);
+                    }
+                    //将重复文件剪切到指定文件夹
+                    try
+                    {
+                        WriteFile(dumpfilefolderName + @"\movelog.txt", text + "来自" + NextFile.FullName);
+                        NextFile.MoveTo(dumpfilefolderName + @"\" + NextFile.Name);
+                        dupfiles++;
+                        savebit += NextFile.Length;
+                    }
+                    catch { }
+                }
+
+                // this.listBox2.Items.Add();
+            }
+            //递归遍历文件夹
+
+            foreach (DirectoryInfo NextFolder in dirInfo)
+            {
+                dealfile(NextFolder);
+            }
+
+        }
+        public void WriteFile(string filePath,string text)
+        {
+            FileStream fs = new FileStream(filePath, FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+            sw.WriteLine(text);
+            sw.Close();
+            fs.Close();
+            Thread.Sleep(20);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
