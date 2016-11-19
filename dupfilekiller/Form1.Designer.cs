ce0965cac2bde47966776092b96570885b7c12de
zwxscience <zwxscience@gmail.com>
2016-11-19 20:03:08 +0800

    change file story method. add resume moved files.
    Signed-off-by: zwxscience <zwxscience@gmail.com>

--- dupfilekiller/Form1.Designer.cs
+++ dupfilekiller/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -74,11 +75,22 @@
             this.label2.Text = "所有的重复文件会剪切在所选择根目录文件夹的\r\ndupfileToDelete文件夹中，如有需要，请手动删除。";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(353, 265);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "恢复重复文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(479, 314);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -97,6 +109,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- dupfilekiller/Form1.cs
+++ dupfilekiller/Form1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
 using System.IO;
+using System.Collections;
+
 namespace dumpfilekiller
 {
     public partial class Form1 : Form
@@ -28,6 +30,7 @@ namespace dumpfilekiller
             FolderBrowserDialog fbd = new FolderBrowserDialog();
             fbd.SelectedPath = "E:\\Music";
             this.textBox1.Text = "";
+            fbd.Description = "请选择待检查文件夹";
             if (fbd.ShowDialog() == DialogResult.OK)
             {
                 //this.textbox1.Text = ;
@@ -41,7 +44,7 @@ namespace dumpfilekiller
                 dealfile(theFolder);
                 fileInfo.Clear();
                 string finalOutput = "共搜索文件 " + fileIndex + " 个,重复文件" + dupfiles + "个,节省空间共约 " + savebit / (1024 * 1024) + "M.";
-                this.textBox1.Text = finalOutput + "\r\n"+ this.textBox1.Text;
+                this.textBox1.Text = finalOutput + "\r\n" + this.textBox1.Text;
                 MessageBox.Show(finalOutput);
             }
 
@@ -67,7 +70,7 @@ namespace dumpfilekiller
                 else
                 {
                     string text = "文件序号" + fileIndex + ":" + NextFile.Name + ",大小:" + NextFile.Length + "字节。";
-                    this.textBox1.Text += text+"是重复文件。\r\n";
+                    this.textBox1.Text += text + "是重复文件。\r\n";
                     if (Directory.Exists(dumpfilefolderName) == false)//如果不存在就创建文件夹
                     {
                         Directory.CreateDirectory(dumpfilefolderName);
@@ -75,8 +78,9 @@ namespace dumpfilekiller
                     //将重复文件剪切到指定文件夹
                     try
                     {
-                        WriteFile(dumpfilefolderName + @"\movelog.txt", text + "保留原始文件"+ fileInfo[fileKey] + ",来自" + NextFile.FullName+"被移出。");
-                        NextFile.MoveTo(dumpfilefolderName + @"\" + NextFile.Name);
+                        WriteFile(dumpfilefolderName + @"\movelog.txt", text + "保留原始文件" + fileInfo[fileKey] + ",来自" + NextFile.FullName + "被移出。");
+                        string fileNewName = NextFile.Name.Substring(0, NextFile.Name.IndexOf(".")) + NextFile.Length + NextFile.Name.Substring(NextFile.Name.IndexOf("."), NextFile.Name.Length - NextFile.Name.IndexOf("."));
+                        NextFile.MoveTo(dumpfilefolderName + @"\" + fileNewName);
                         dupfiles++;
                         savebit += NextFile.Length;
                     }
@@ -93,7 +97,7 @@ namespace dumpfilekiller
             }
 
         }
-        public void WriteFile(string filePath,string text)
+        public void WriteFile(string filePath, string text)
         {
             FileStream fs = new FileStream(filePath, FileMode.Append);
             StreamWriter sw = new StreamWriter(fs, Encoding.Default);
@@ -112,5 +116,109 @@ namespace dumpfilekiller
         {
 
         }
-    }
+
+        Dictionary<string, long> fileSrcList;//key,src fileSrcList;
+        void ReadLog(string path)
+        {
+            fileSrcList = new Dictionary<string, long>();
+            try
+            {
+                FileStream aFile = new FileStream(path, FileMode.Open);
+                StreamReader sr = new StreamReader(aFile, Encoding.GetEncoding("GB2312"));
+                string strLine = sr.ReadLine();
+
+                while (strLine != null)
+                {
+                    try {
+
+                    string fileFullName = strLine.Substring(strLine.IndexOf(",来自") + 3, strLine.IndexOf("被移出。") - strLine.IndexOf(",来自") - 3);
+                    long fileSize = long.Parse(strLine.Substring(strLine.IndexOf("大小:") + 3, strLine.IndexOf("字节。") - strLine.IndexOf("大小:") - 3));
+                    fileSrcList.Add(fileFullName, fileSize);
+                    }
+                    catch { }
+                    strLine = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("An IOException has been thrown!");
+                return;
+            }
+
+
+        }
+
+        ArrayList fileToDel;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(dumpfilefolderName) == false )//如果不存在就选择
+            {
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                fbd.SelectedPath = "E:\\Music";
+                fbd.Description = "请选择待恢复文件夹";
+                this.textBox1.Text = "";
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    dumpfilefolderName = fbd.SelectedPath + "\\dupfileToDelete";
+                }
+            }
+            if (Directory.Exists(dumpfilefolderName) == false || !File.Exists(dumpfilefolderName + "\\movelog.txt"))
+            {
+                dumpfilefolderName = "";
+                MessageBox.Show("文件夹待恢复数据不存在或已删除。");
+                return;
+            }
+                ReadLog(dumpfilefolderName + "\\movelog.txt");
+                if (fileSrcList.Count != 0)
+                {
+                    int Rcount = 0;
+                    fileToDel = new ArrayList();
+                    foreach (KeyValuePair<string, long> fileP in fileSrcList)
+                    {
+                        try
+                        {
+                            string filePath = fileP.Key;
+                            string fileName = filePath.Substring(filePath.LastIndexOf("\\")+1, filePath.Length - filePath.LastIndexOf("\\")-1);
+                            string fileNameInFolder = fileName.Substring(0, fileName.IndexOf(".")) + fileP.Value + fileName.Substring(fileName.IndexOf("."), fileName.Length - fileName.IndexOf("."));
+                            File.Copy(dumpfilefolderName + "\\"+fileNameInFolder, filePath);
+                            fileToDel.Add(dumpfilefolderName + "\\" + fileNameInFolder);
+                            Rcount++;
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                    MessageBox.Show("成功恢复文件 "+Rcount+" 个，失败 "+(fileSrcList.Count-Rcount) +"个。");
+                    //删除恢复成功的备份文件
+                    foreach (string file in fileToDel)
+                    {
+                        try
+                        {
+                            File.Delete(file);
+                        }
+                        catch { }
+                    }
+                //清空log文件
+                try
+                {
+                    File.Delete(dumpfilefolderName + "\\movelog.txt");
+                }catch { }
+                
+                
+                if (fileToDel!=null)
+                    fileToDel.Clear();
+
+                }
+                else
+                {
+                MessageBox.Show("未找到需要恢复的文件。");
+            }
+        
+        if(fileSrcList!=null)
+        fileSrcList.Clear(); 
+
+        }
+}
 }
